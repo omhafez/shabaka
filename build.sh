784aee570ba3919913df7a3814ce9df665a49c60
Omar Hafez <ohafez@apple.com>
2020-03-25 13:45:23 -0700

    fixing nlopt and vtk for Ubuntu 19.10

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ set -e
 SECONDS=0
 
 # ============================================================================
-# 
+#
 # SHABAKA
 # Image-Based Mesh Generation
 # ----------------------------------------------------------------------------
-# 
+#
 # Read installation instructions in README before running this script!
-# 
+#
 # This script must be run using sudo. Most commands do not use superuser
 # privileges, but some require it.
 
@@ -41,16 +41,16 @@ if [ $UNAME_S == "Darwin" ]; then
 
    # GCC 6.3.0
    $NOTSUDO brew reinstall gcc
-   
+
    # GNU SED
    $NOTSUDO brew reinstall gnu-sed
-   
+
    # WGET 1.14
    $NOTSUDO brew reinstall wget
-   
+
    # CMAKE 3.7.2
-   $NOTSUDO brew reinstall cmake  
-   
+   $NOTSUDO brew reinstall cmake
+
    # VTK 8.2.0
    $NOTSUDO brew reinstall vtk
 
@@ -98,7 +98,7 @@ if [ $UNAME_S == "Darwin" ]; then
    $NOTSUDO wget "https://github.com/SCIInstitute/Seg3D/releases/download/v2.4.4/Seg3D2-2.4.4-Darwin.pkg"
    sudo installer -pkg Seg3D2-2.4.4-Darwin.pkg -target /
    sudo rm -rf Seg3D2-2.4.4-Darwin.pkg
-   
+
    # TETGEN 1.5.0
    $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/mac/tetgen1.5.1.zip" -O src/external/tetgen1.5.1.zip
    cd src/external
@@ -106,23 +106,23 @@ if [ $UNAME_S == "Darwin" ]; then
    $NOTSUDO rm -rf tetgen1.5.1.zip
    $NOTSUDO make -j4 -C tetgen1.5.1
    cd -
-   
+
    # GMSH 2.16
    $NOTSUDO brew reinstall gmsh
-   
+
    # SHABAKA
    export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/libexec:$LD_LIBRARY_PATH
-   
+
    $NOTSUDO make clean -C $shabaka_dir/src/ptcloudgen/
    $NOTSUDO make clean -C $shabaka_dir/src/surfrecon/voronoi/vsitegen/
    $NOTSUDO make clean -C $shabaka_dir/src/surfrecon/voronoi/qvor2vtk/
    $NOTSUDO make clean -C $shabaka_dir/src/pmeshgen/ply2smfe/
-   
+
    $NOTSUDO make -C $shabaka_dir/src/ptcloudgen/
    $NOTSUDO make -C $shabaka_dir/src/surfrecon/voronoi/vsitegen/
    $NOTSUDO make -C $shabaka_dir/src/surfrecon/voronoi/qvor2vtk/
    $NOTSUDO make -C $shabaka_dir/src/pmeshgen/ply2smfe/
-   
+
    echo ""
    echo "------------------------------------------------------------------------------ "
    echo "ALL DONE!"
@@ -130,7 +130,7 @@ if [ $UNAME_S == "Darwin" ]; then
    export elapsed
    echo "import os; print 'ELAPSED TIME:', format(float(os.environ['elapsed'])/60., '.2f'), 'minutes'" | python
    echo ""
-  
+
 # ----------------------------------------------------------------------------
 # LINUX
 # ----------------------------------------------------------------------------
@@ -153,8 +153,8 @@ elif [ $UNAME_S == "Linux" ]; then
    sudo apt-get -y install wget
 
    # CMAKE 2.8.12
-   sudo apt-get -y install cmake  
-   
+   sudo apt-get -y install cmake
+
    # VTK 6.0
    sudo apt-get -y install vtk6 libvtk6-dev
 
@@ -163,19 +163,27 @@ elif [ $UNAME_S == "Linux" ]; then
    sudo ln -sf /usr/bin/teem-unu /usr/bin/unu
 
    # NLOPT 2.4.1
-   sudo apt-get -y install libnlopt-dev
+   $NOTSUDO cd src/external
+   $NOTSUDO git clone git://github.com/stevengj/nlopt
+   $NOTSUDO cd nlopt
+   $NOTSUDO mkdir build
+   $NOTSUDO cd build
+   $NOTSUDO cmake ..
+   $NOTSUDO make
+   sudo make install
+   $NOTSUDO cd -
 
    # QHULL 2015.2
    $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/linux/qhull-2015-src-7.2.0.tgz"
    $NOTSUDO tar -xvf qhull-2015-src-7.2.0.tgz -C src/external
    $NOTSUDO make -C src/external/qhull-2015.2
-   $NOTSUDO rm qhull-2015-src-7.2.0.tgz*  
+   $NOTSUDO rm qhull-2015-src-7.2.0.tgz*
 
    # ACVD 2.0
    $NOTSUDO git clone https://github.com/valette/ACVD src/external/ACVD
    cd src/external/ACVD
    export LD_LIBRARY_PATH=/usr/local
-   if [ $MYVERSION == "16.04" ]; then
+   if [ $MYVERSION == "16.04" ] || [ $MYVERSION == "19.10" ]; then
       sudo ln -sf /usr/bin/vtk6 /usr/bin/vtk
    fi
    $NOTSUDO cmake . -DCMAKE_BUILD_TYPE=Release
@@ -188,41 +196,41 @@ elif [ $UNAME_S == "Linux" ]; then
    $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/linux/ParaView-4.0.1-Linux-64bit.tgz"
    $NOTSUDO tar -xvf ParaView-4.0.1-Linux-64bit.tgz -C src/external
    $NOTSUDO rm ParaView-4.0.1-Linux-64bit.tgz*
-   
+
    # TETGEN 1.5.0
    sudo apt-get -y install tetgen
-   
+
    # GMSH 3.0.1
    $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/linux/gmsh-3.0.1-Linux64.tgz"
    $NOTSUDO tar -xvf gmsh-3.0.1-Linux64.tgz -C src/external
    $NOTSUDO rm gmsh-3.0.1-Linux64.tgz*
-   
+
    # SHABAKA
    $NOTSUDO make clean -C $shabaka_dir/src/ptcloudgen/
    $NOTSUDO make clean -C $shabaka_dir/src/surfrecon/voronoi/vsitegen/
    $NOTSUDO make clean -C $shabaka_dir/src/surfrecon/voronoi/qvor2vtk/
    $NOTSUDO make clean -C $shabaka_dir/src/pmeshgen/ply2smfe/
-   
+
    $NOTSUDO make -C $shabaka_dir/src/ptcloudgen/
    $NOTSUDO make -C $shabaka_dir/src/surfrecon/voronoi/vsitegen/
    $NOTSUDO make -C $shabaka_dir/src/surfrecon/voronoi/qvor2vtk/
    $NOTSUDO make -C $shabaka_dir/src/pmeshgen/ply2smfe/
    echo ""
-   
+
    if grep -q Microsoft /proc/version; then
       ## WINDOWS WSL
-      
+
       # MESHLAB 2016.12
       sudo add-apt-repository -y ppa:zarquon42/meshlab
       sudo apt-get -y update
       sudo apt-get -y install meshlab
-      
+
       # DOWNLOAD WINDOWS EXECUTABLES
       $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/windows/Seg3D2-2.3.2-win64.exe"
       $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/windows/MeshLab2016.12.exe"
       $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/windows/ParaView-5.2.0-Qt4-OpenGL2-Windows-64bit.exe"
       $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/windows/Xming-6-9-0-31-setup.exe"
-      $NOTSUDO mkdir src/external/windows 
+      $NOTSUDO mkdir src/external/windows
       $NOTSUDO mv *.exe src/external/windows
 
       echo "------------------------------------------------------------------------------ "
@@ -231,32 +239,32 @@ elif [ $UNAME_S == "Linux" ]; then
       export elapsed
       echo "import os; print 'ELAPSED TIME:', format(float(os.environ['elapsed'])/60., '.2f'), 'minutes'" | python
       echo ""
-   
+
    else
       ## UBUNTU
-      
+
       # MESHLAB 2016.12
       sudo add-apt-repository -y ppa:zarquon42/meshlab
       sudo apt-get -y update
       sudo apt-get -y install meshlab
 #       sudo apt-get -y install snapd
 #       sudo snap install meshlab
-   
+
       # SEG3D 2.1.4
       if [ $MYVERSION == "14.04" ]; then
          sudo apt-get -y install libinsighttoolkit3.20 libtet1.5
          $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/linux/seg3d_2.1.4-0ubuntu0ppa3_amd64.deb"
          sudo dpkg -i seg3d_2.1.4-0ubuntu0ppa3_amd64.deb
          sudo rm seg3d_2.1.4-0ubuntu0ppa3_amd64.deb*
-      fi  
-         
+      fi
+
       # ADD DESKTOP FILES
       $NOTSUDO wget "https://github.com/omhafez/shabaka-externals/raw/master/linux/desktop.tgz"
       $NOTSUDO tar -xf desktop.tgz
       sudo mv desktop/*.png /usr/share/icons
       sudo mv desktop/*.desktop ~/.local/share/applications
       $NOTSUDO rm -rf desktop desktop.tgz*
-         
+
       echo "------------------------------------------------------------------------------ "
       echo "ALL DONE!"
       elapsed=$SECONDS
@@ -265,8 +273,8 @@ elif [ $UNAME_S == "Linux" ]; then
       echo ""
 
    fi
-   
-   
+
+
 # ----------------------------------------------------------------------------
 # UNSUPPORTED OS
 # ----------------------------------------------------------------------------
